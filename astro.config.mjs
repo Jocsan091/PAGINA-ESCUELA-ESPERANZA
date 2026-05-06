74ea94c21b473001af68e767b557ad44827e0e43
Rodri <rodrigodemonte63@gmail.com>
2026-05-06 02:08:25 -0400

    limpiar configuracion cloudflare pages estatica

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
-  trailingSlash: "ignore",
-  adapter: cloudflare()
+  trailingSlash: "ignore"
 });
